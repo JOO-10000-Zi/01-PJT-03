23ddddec6fa0323e79b1e906165e6fde1ee424ac
JOO-10000-Zi <j4615545@gmail.com>
2022-07-29 17:55:22 +0900

    다시 저상

--- 1회차/주세환/0_최빈수구하기.py
+++ 1회차/주세환/0_최빈수구하기.py
@@ -2,21 +2,3 @@ import sys
 
 sys.stdin = open("_최빈수구하기.txt")
 
-
-T = int(input())
-# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
-for test_case in range(1, T + 1):
-    sagak = list(map(int, input().split()))
-    line_s = []
-    shout = []
-    for i in sagak:
-        line_s.append(sagak.count(i))
-    for i in line_s:
-            for  j in line_s:
-                if i > 2 or i < 2 :
-                    shout.append(sagak[j])
-    print(shout)
-# 수가 몇 번 있는지 카운트 한 후 
-# 수가 2보다 작고 2보다 큰경우 값을 출력 할려고 했는데
-# 마지막에서 막혔습니다...
-# 딕셔너리 값 추가하는 방법이 생각이 안나서 결국 리스트로....
--- 1회차/주세환/1_소득불균형.py
+++ 1회차/주세환/1_소득불균형.py
@@ -16,3 +16,5 @@ for test_case in range(1, T + 1):
             cnt += 1
      
     print(f'#{test_case} {cnt}')
+
+    
