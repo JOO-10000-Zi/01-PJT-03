2718587a90fcf678b5027b3457e498f96f893a6f
JOO-10000-Zi <j4615545@gmail.com>
2022-07-31 00:19:09 +0900

    틀린 문제 풀이

--- 1회차/주세환/3_직사각형길이찾기.py
+++ 1회차/주세환/3_직사각형길이찾기.py
@@ -7,15 +7,16 @@ T = int(input())
 # 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
 for test_case in range(1, T + 1):
     sagak = list(map(int, input().split()))
-    line_s = []
-    shout = []
+    
+    line_ = {}
     for i in sagak:
-        line_s.append(sagak.count(i))
-    for i in line_s:
-            for  j in line_s:
-                if i > 2 or i < 2 :
-                    shout.append(sagak[j])
-    print(shout)
+        if i not in line_:
+            line_[i] = 1
+        else:
+            line_[i] += 1
+            
+    print(line_)
+    print(f'#{test_case} {min(line_, key=line_.get)}')
 # 수가 몇 번 있는지 카운트 한 후 
 # 수가 2보다 작고 2보다 큰경우 값을 출력 할려고 했는데
 # 마지막에서 막혔습니다...
